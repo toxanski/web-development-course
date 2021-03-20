39a1be50e32a1886713855ac672c492e7ff5c225
Smirnov A.A <tosha.smirnov.98@gmail.com>
2021-03-21 01:08:21 +0300

    added data formatting

--- lw3/task2/script.js
+++ lw3/task2/script.js
@@ -2,20 +2,21 @@ var isTimeRangesIntersect = function (timeRange1, timeRange2) {
   if (Array.isArray(timeRange1) && Array.isArray(timeRange2)) {
 
     var arr = timeRange1.concat(timeRange2);
+		for (var item of arr) {
+			if (item[2] !== ':') {
+				return console.log('false');
+			}
+		}
+
     let newArr = [];
     for (var item of arr) {
       var str = +item.replace(/[:]/, "");
       newArr.push(str);
     }
-
-    if (newArr[0] <= newArr[3] && newArr[1] >= newArr[2]) {
-      console.log('true');
-    } else {
-      console.log('false');
-    }
+		(newArr[0] <= newArr[3] && newArr[1] >= newArr[2]) ? console.log('true') : console.log('false');
   } else {
     console.log('false (Введите два массива!)');
   }
 }
-isTimeRangesIntersect(['08:30', '09:30'], ['10:30', '12:00']); // return false
+isTimeRangesIntersect(['08:30', '11:30'], ['10:30', '12:00']); // return false
 isTimeRangesIntersect(['18:30', '19:30'], ['19:00', '21:00']); // return true
