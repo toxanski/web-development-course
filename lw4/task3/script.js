81f4678c3a2713d86a2038d4a09654375f27cce0
Smirnov A.A <tosha.smirnov.98@gmail.com>
2021-04-14 00:24:21 +0300

    fixed merge branches

--- lw4/task3/script.js
+++ lw4/task3/script.js
@@ -1,5 +1,4 @@
 Array.prototype.forEachRight = function(callback) {
-<<<<<<< HEAD
   for (let i = this.length - 1; i >= 0; i--) {
     callback(this[i], i, this);
   }
@@ -8,14 +7,4 @@ Array.prototype.forEachRight = function(callback) {
 const myArr = ['path', 'fs', 'os', 'events'];
 myArr.forEachRight(function(item, i, arr) {
   console.log(item, i);
-=======
-  for (let i = 0; i < this.length; i++) {
-    callback(this, i, this[i]);
-  }
-}
-
-myArr = ['path', 'fs', 'os'];
-myArr.forEachRight(function(arr, i, item) {
-  console.log(arr, i, item);
->>>>>>> 7df762d8dad9ea2647057763e41fd2cf69075fc4
 });
